34e983afbb272b83d1255b237e71d4ac36c0b57a
Asad Ahmad <aa18514@ic.ac.uk>
2016-07-22 14:16:41 +0100

    created the structure for the raw data recieved by the pc /13

--- pc_data_dump.h
+++ pc_data_dump.h
@@ -5,18 +5,17 @@
 
 union pc_data
 [
-			struct
-			{
-				uint8_t		id
-				unsigned 	time;
-				uint8_t		n-fib; 
-				uint8_t		n-fob; 
-				uint8_t		n-fsc;
-				uint8_t 	sci-fib[n-fib*FIB_SCI_DATA_SIZE];
-				uint8_t		sci-fob[n-fob*FOB_SCI_DATA_SIZE];
-				uint8_t		sci-fsc[n-fsc*FSC_SCI_DATA_SIZE];
-				
-			};
-			uint8_t pc_pckt[8 + n-fib*FIB_SCI_DATA_SIZE + n-fob*FOB_SCIE_DATA_SIZE + n-fsc*FSC_SCI_DATA_SIZE]; 
+	struct
+	{
+		uint8_t		id
+		unsigned 	time;
+		uint8_t		n-fib; 
+		uint8_t		n-fob; 
+		uint8_t		n-fsc;
+		uint8_t 	sci-fib[n-fib*FIB_SCI_DATA_SIZE];
+		uint8_t		sci-fob[n-fob*FOB_SCI_DATA_SIZE];
+		uint8_t		sci-fsc[n-fsc*FSC_SCI_DATA_SIZE];	
+	};
+	uint8_t pc_pckt[8 + n-fib*FIB_SCI_DATA_SIZE + n-fob*FOB_SCIE_DATA_SIZE + n-fsc*FSC_SCI_DATA_SIZE]; 
 
 ]
