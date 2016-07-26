2890dca9d34a8ccac41e87961b9600e6bb8b4363
Asad Ahmad <aa18514@ic.ac.uk>
2016-07-26 12:28:05 +0100

    changes made to timer_isr

--- python/test.py
+++ python/test.py
@@ -7,14 +7,15 @@ def send_start_command_to_arduino(serial_port):
 	time_out = 1
 	time.sleep(time_out)
 	serial_port.setDTR(0)
-	time.sleep(time_out)
 	serial_port.write(b'5')
-	serial_port.flushInput()
 	time.sleep(time_out)
 	
 	
 if __name__ == "__main__":
 	baud_rate = 250000 
+	created = {false, false, false }
+	file = open("newfile.csv", 'w')
+	file.write("hello world in another line")
 	parser = argparse.ArgumentParser(); 
 	parser.add_argument(dest = 'port', help = "display the interface port to the computer ", type = str)
 	args = parser.parse_args() 
@@ -23,8 +24,13 @@ if __name__ == "__main__":
 	send_start_command_to_arduino(serial_port)
 	while 1: 
 		try: 	
-			if(serial_port.read != ''):
-				print (serial_port.read(size = 7))
+			data = (serial_port.read(size = 1))
+			if(data[0] == 1 && created[0] == false):
+				string file = "sci_fib"
+				created[0] = true
+				with open(file, "w") as f: 
+					f.write("HELLO")
+			print(data)
 		except serial.SerialTimeoutException:
 			print("DATA COULD NOT BE READ")  
 	
