3159cbdc0124ad8d5fbdc999d68596ec2f92b888
Asad Ahmad <aa18514@ic.ac.uk>
2016-07-27 13:18:30 +0100

    prototype for getting the x, y and z cordinates out, testing required, optimized time_update, need to change format in excel

--- python/test.py
+++ python/test.py
@@ -2,23 +2,8 @@ import serial
 import argparse
 import string
 import time 
-import base64
 import datetime
-def send_start_command_to_arduino(serial_port): 
-	time_out = 1
-	time.sleep(time_out)
-	serial_port.setDTR(0)
-	time.sleep(time_out)
-	serial_port.write(b'5')
-	serial_port.flushInput()
-	time.sleep(time_out)
 
-def write_to_file(file, data, open, time):
-	if(data != 0 and file == ''): 
-		print('H')
-		data = data - 1
-		return str(open + time + ".csv")
-	
 if __name__ == "__main__":
 	# args = parser.parse_args() 
 	# port = args.port 
@@ -26,12 +11,6 @@ if __name__ == "__main__":
 	# send_start_command_to_arduino(serial_port)
 	# while 1: 
 		# try: 	
-			# data = (serial_port.read(size = 18))
-			# timestr = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-			# file_name[0] = write_to_file(file_name[0], data[9], "fib_sci", timestr)
-			# file_name[1] = write_to_file(file_name[1], data[10], "fob_sci", timestr)
-			# file_name[2] = write_to_file(file_name[2], data[11], "fsc_sci", timestr)
-			# print (file_name[1]);
 			# with open((file_name[1]), "a") as f: 
 					# f.write("fsc_sci_" + timestr)
 		# except serial.SerialTimeoutException:
@@ -45,23 +24,52 @@ if __name__ == "__main__":
 	
 	# arduino startup time
 	time.sleep(1)
-	
+	science_data = [0, 0, 0]
+	x_hb = [0, 0, 0]
+	y_hb = [0, 0, 0]
+	z_hb = [0, 0, 0]
 	t = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 	s.write(b'5')
-	with open("fib_sci_" + t + ".csv", 'a') as f, open ("fob_sci_" + t + ".csv", 'a') as se, open ("fsc_sci_" + t + ".csv", 'a') as d:
-		f.write("date" + "," +  "time" + "," +  "sync_counter" + "\n")
-		d.write("date" + "," +  "time" + "," +  "sync_counter" + "\n")
-		se.write("date" + "," +  "time" + "," +  "sync_counter" + "\n")
+	with open("fib_sci_" + t + ".csv", 'a') as f, open ("fob_sci_" + t + ".csv", 'a') as se,  open ("fsc_sci_" + t + ".csv", 'a') as d:
+		f.write("status" + "," + "date" + "," +  "time" + "," +  "sync_counter" + "\n")
+		d.write("status" + "," + "date" + "," +  "time" + "," +  "sync_counter" + "\n")
+		se.write("status" + "," + "date" + "," +  "time" + "," +  "sync_counter" + "\n")
+		current_time = datetime.datetime.now()
 		while True:
 			try:
-				#a = 2
 				data = s.read(size = 18)
 				s.flushInput()
-				#hex_string = "".join("%00x" % b for b in data)
-				#print(data[1:5]);
-				f.write(datetime.datetime.now().strftime("%Y-%m-%d") + ","  + datetime.datetime.now().strftime("%H:%M:%S.%f") +  "," + ("{}".format(int.from_bytes(data[1:5], byteorder  = 'little'))) + "\n")
-				d.write(datetime.datetime.now().strftime("%Y-%m-%d") + ", " + datetime.datetime.now().strftime("%H:%M:%S.%f") +  ", " + ("{}".format(int.from_bytes(data[1:5], byteorder = 'little'))) + "\n")
-				se.write(datetime.datetime.now().strftime("%Y-%m-%d")+ ", " + datetime.datetime.now().strftime("%H:%M:%S.%f") +  ", " + ("{}".format(int.from_bytes(data[1:5], byteorder = 'little'))) + "\n")
+				status = str(data[0]);
+				
+				fib_lower_limit = 8; 
+				fib_upper_limit = fib_lower_limit + 10*data[5] - 1; 
+				if(fib_lower_limit < fib_upper_limit):
+					science_data[0] = ("{}".format(int.from_bytes(data[fib_lower_limit:fib_upper_limit], 'little')))
+					
+				fob_lower_limit = fib_lower_limit + 10*data[5] 
+				fob_upper_limit = fib_lower_limit + 10*data[5] + 10*data[6] - 1;
+				if(fob_lower_limit < fob_upper_limit): 
+					science_data[1] = ("{}".format(int.from_bytes(data[fob_lower_limit:fob_upper_limit], 'little')))
+				
+				fsc_lower_limit = fob_upper_limit + 1
+				fsc_upper_limit = fsc_lower_limit + 10*data[7]  - 1; 
+				if(fsc_lower_limit < fsc_upper_limit): 
+					science_data[2] = ("{}".format(int.from_bytes(data[fsc_lower_limit:fsc_upper_limit], 'little')))
+	
+				for i in range(0, 3):
+					if(data[i+5] > 0 ): 
+						x_hb[i] = int(science_data[i]) & 0x00000001
+						y_hb[i] = int(science_data[i]) & 0x00000003
+						z_hb[i] = int(science_data[i]) & 0x00000006
+				
+				if(data[0] > 0):
+					f.write(status + "," + current_time.strftime("%Y-%m-%d")  + " , "  + current_time.strftime("%H:%M:%S.%f") +  "," + ("{}".format(int.from_bytes(data[1:5], byteorder  = 'little'))) + "\n") 
+				if(data[1] > 0):
+					d.write(status + "," + current_time.strftime("%Y-%m-%d")  + " , " + current_time.strftime("%H:%M:%S.%f") +  ", " + ("{}".format(int.from_bytes(data[1:5], byteorder = 'little'))) + "\n") 
+				if(data[2] > 0):
+					se.write(status + "," + current_time.strftime("%Y-%m-%d") + " , " + current_time.strftime("%H:%M:%S.%f") +  ", " + ("{}".format(int.from_bytes(data[1:5], byteorder = 'little'))) + "\n")
+				
+				current_time = current_time + datetime.timedelta(milliseconds = 7.8125)					#frequency set at 128 Hz, timedelta = 1/frequency ~ 7.8125 milliseconds
 			except KeyboardInterrupt:
 				break;
 
