8825b2d30e6c64c976022ac15166ded35a7c5f8b
Asad Ahmad <aa18514@ic.ac.uk>
2016-08-01 12:53:46 +0100

    updated python code

--- python/test.py
+++ python/test.py
@@ -8,13 +8,15 @@ from abc import ABCMeta, abstractmethod
 
 class fee_packet: 
 	science_data=['','','']
-	def update(self, data, id, time, n_fib, n_fob, n_fsc):
-		self.id 				= id
-		self.data 			= data
-		self.time 			= time
-		self.n_fib 			= n_fib
-		self.n_fob 			= n_fob
-		self.n_fsc 			= n_fsc 
+	def update(self, data):			
+		self.data = data
+		self.id	= str(data[0])
+		self.sync_counter   = ("{}".format(int.from_bytes(data[1 : 5], byteorder = 'big'))); 
+		delta_val 			= float(self.sync_counter) * 7.8125 
+		self.time =          current_time + datetime.timedelta(milliseconds = delta_val) ;
+		self.n_fib 			= data[5]
+		self.n_fob 			= data[6]
+		self.n_fsc 			= data[7] 
 		self.n_lower_limit 	= 8  
 		
 	def get_nfib(self): 
@@ -27,18 +29,18 @@ class fee_packet:
 	def update_time(self): 
 		sync_counter = ("{}".format(int.from_bytes(self.data[1 : 5], byteorder = 'big'))); 
 		delta_val = sync_counter * 7.8125 
-		self.time =  self.time +datetime.timedelta(milliseconds = delta_val)
+		return (self.time +datetime.timedelta(milliseconds = delta_val))
 		
 
 	#@abstractmethod
 	def store_to_pc(self, file): 
-		file.write((self.time.strftime("%Y%m%d-%H%M%S")) + "," +  str(self.id) + "," + str(self.n_fsc) + "," + str(self.n_fob) + "," + str(self.n_fsc) + ",")
+		file.write((self.time.strftime("%Y%m%d %H:%M:%S.%f")) + "," +  str(self.id) + "," + str(self.n_fsc) + "," + str(self.n_fob) + "," + str(self.n_fsc) + ",")
 
 class fsc_packet(fee_packet):
 	#fsc_packet inherits from fee_packet and therefore it should have the attributes id, data, time, n_fib, n_fob, n_fsc and n_lower_limit 
 		
 	def __init__(self, data, time): 
-		fee_packet.update(self, data, data[0], time, data[5], data[6], data[7])
+		fee_packet.update(self, data)
 		self.n_upper_limit					= self.n_lower_limit + data[5]*10 + data[6]*10 + data[7]*10
 		self.science_data[2] = bytearray(self.data[self.n_lower_limit : self.n_upper_limit])
 		self.__sensor_temp_controller 			= self.science_data[2][0] & 0xF0
@@ -66,7 +68,7 @@ class fib_packet(fee_packet):
 		self.__z 							=   ("{}".format(int.from_bytes([z_hlb, z_mb, z_hlb], byteorder = 'little')))
 				
 	def __init__(self, data, time): 
-		fee_packet.update(self, data,data[0], time, data[5], data[6], data[7])
+		fee_packet.update(self, data)
 		self.n_upper_limit				= 	self.n_lower_limit + data[5]*10
 		self.science_data[0] = bytearray(self.data[self.n_lower_limit : self.n_upper_limit])
 		self.update_xyz( data, str(data[0]), str(time), data[5], data[6], data[7], fee_packet.science_data[0][0], fee_packet.science_data[0][1], fee_packet.science_data[0][2], fee_packet.science_data[0][3], fee_packet.science_data[0][4], fee_packet.science_data[0][5], fee_packet.science_data[0][6], fee_packet.science_data[0][7])
@@ -83,7 +85,7 @@ class fob_packet(fee_packet):
 		self.__z 							=   ("{}".format(int.from_bytes([z_hb, z_mb, z_lb], byteorder = 'little')))
 		self.__sensor_range 				= 	sensor_range
 	def __init__(self, data, time): 
-		fee_packet.update(self, data,data[0], time, data[5], data[6], data[7])
+		fee_packet.update(self, data)
 		self.n_upper_limit				= 	self.n_lower_limit + data[5]*10
 		self.science_data[1] = bytearray(self.data[self.n_lower_limit : self.n_upper_limit])
 		self.update_xyz( data, str(data[0]), str(time), data[5], data[6], data[7], fee_packet.science_data[1][2], fee_packet.science_data[1][1], fee_packet.science_data[1][0], fee_packet.science_data[1][5], fee_packet.science_data[1][4], fee_packet.science_data[1][3], fee_packet.science_data[1][8], fee_packet.science_data[1][7], fee_packet.science_data[1][6], fee_packet.science_data[1][9])
@@ -113,9 +115,8 @@ if __name__ == "__main__":
 	science_data = [0, 0, 0]
 	data = 0
 	
-	s = serial.Serial(args.port, baud_rate, timeout = 1)				
-	fee_pack = fee_packet();
-	fee_pack.update(data, str(data[0]), current_time, data[5], data[6], data[7]) 
+	s = serial.Serial(args.port, baud_rate, timeout = 1)
+	
 	# arduino startup time
 	time.sleep(1)
 
@@ -137,19 +138,27 @@ if __name__ == "__main__":
 			try:
 				data = bytearray(s.read(size = 28))
 				s.flushInput()			#debug_information(data)
+				
+				fee_pack = fee_packet();
+				fee_pack.update(data) 
 				#print(fee_pack.get_nfib())
 				if(fee_pack.get_nfib() > 0): 
 					fee_pack = fib_packet(data, time)
 					fee_pack.store_to_pc(f)
 				
-				#if(fee_pack.get_nfob() > 0): 
-				#	fee_pack = fob_packet(data, time)
-			#		fee_pack.store_to_pc(se)
+				if(fee_pack.get_nfob() > 0): 
+					fee_pack = fob_packet(data, time)
+					fee_pack.store_to_pc(se)
 					
 				if(fee_pack.get_nfsc() > 0): 
 					fee_pack = fsc_packet(data, time) 
 					fee_pack.store_to_pc(d)
-				fee_pack.update_time();					
+				
+				
+				#get the value of sync_counter from data, the value of current_time never changes. 
+				#we use the formula, time = current_time(constant) + sync_counter * 7.8125 ms (where sync_counter is the variable)
+					
+				
 			except KeyboardInterrupt:
 				break;
 
