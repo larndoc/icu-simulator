b66c8b73f22ca7021d8e68dd627b7f49b04c7bf5
Henry Eshbaugh <he915@ic.ac.uk>
2016-08-15 16:28:39 +0100

    FSC panel.
    Mind that headers in python/data for FSC stuff seem to be malformed
    and will break the script.

---
+++ python/fsc/main.py
@@ -0,0 +1,25 @@
+from bokeh.plotting import curdoc
+from bokeh.layouts  import gridplot
+import sys
+sys.path.append("..")
+import jmag_bokeh_if as jbif
+
+fft_dc = jbif.FFT_Cooker()
+ff     = jbif.Figure_Factory(x_axis_type='linear')
+
+ts = jbif.Grapher(jbif.get_latest_csv("data/FSC_Sci_*.csv"), key_groups=[['Sensor_Laser'], ['Laser_Micro'], ['Zeeman'], ['Sci_Data']])
+sp = jbif.Grapher(jbif.get_latest_csv("data/FSC_Sci_*.csv"), cooker=fft_dc, indep_var='Freq', key_groups=[['Sci_Data']], figure_factory=ff)
+
+timeseries = ts.make_new_graphs()
+spectra    = sp.make_new_graphs()
+
+l = gridplot(
+       [[timeseries[0], None],
+        [timeseries[1], None],
+        [timeseries[2], None],
+        [timeseries[3], spectra[0]]]
+        )
+
+curdoc().add_root(l)
+curdoc().add_periodic_callback(ts.update_graphs, 100)
+curdoc().add_periodic_callback(sp.update_graphs, 500)
