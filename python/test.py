ce4117f5f9f40d2f24f7e065ccd023bde817e13e
aa18514 <aa18514@ic.ac.uk>
2016-08-23 11:26:54 +0100

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,7 +1,6 @@
 import serial 
 import argparse
 import datetime
-import collections
 import logging
 import time
 import binascii
@@ -32,35 +31,23 @@ def build_fee_packet(fee_number):
 class hk_data: 	
 		def __init__(self, port): 
 			self.port = port
-		def update(self, current_time, counter, values):
-			time = (current_time + datetime.timedelta(seconds = counter/128)).strftime("%Y-%m-%dT%H:%M:%S.%f")
-			hk_packets = [pcu_data(self.port.read(size = 32)), fib_hk_data(self.port.read(size = 40)), fob_hk_data(self.port.read(size = 4)), fsc_hk_data(self.port.read(size = 53))]
+		def update(self, values):
+			#values['house_keeping'] = '' 
+			hk_packets = [self.port.read(size = 32), self.port.read(size = 40), self.port.read(size = 4), self.port.read(size = 53)]
 			i = 0
-			for key in values: 
-				values[key] = time + ',' +  hk_packets[i].update() + '\n'
+			for i in range(0, 4): 
+				values['house_keeping'] += hk_packets[i]
 				i += 1
-				
 		 
 class packet_reciever(Thread):
 	#recieves a packet and reads the first byte 
-	values = (('pcu_data', []), ('fib_hk_tm', []))
-	hk_values = (('pcu_data',''), ('fib_hk_tm',''), ('fob_hk_tm', ''), ('fsc_hk_tm', ''))
-	sci_values = (('fib_sci_tm', ''), ('fob_sci_tm', ''), ('fsc_sci_tm', ''))
-	hk_values = collections.OrderedDict(hk_values)
-	sci_values = collections.OrderedDict(sci_values) 
+	
+	hk_values = dict()
+	sci_values = dict() 
 	files   = dict()
-	time = ''
 	start_running = True
-	labels = dict()
 	
 	def __init__(self, serial_port, logdir): 
-		self.labels = {'fib_sci_tm' : ['Time', 'Bx', 'By', 'Bz', 'status'], 
-			       'fob_sci_tm'  : ['Time', 'Bx', 'By', 'Bz'],
-			       'fsc_sci_tm' : ['Time',	'Sensor_Laser',	'Laser_Micro', 'Zeeman','Sci_Data_ID','Sci_Data','Timestamp'],
-			       'fib_hk_tm' : ['Time','Status_HB','Status_LB','LUP_cnt','P1V5','P3V3','P5V','N5V','P8V','P12V','P1I5','P3I3','P5I','N5I','P8I','P12I','TE','TS1','TS2','Checksum','CMD_exec_cnt','ICU_err_cnt','Last_cmd','Last_err','HK_req_cnt','sync_count']
-					   ,'fob_hk_tm' : ['Time','Delay/Advance Val', 'ICU packet checksum', 'ICU command count', 'sync_pulse_count']
-					   ,'fsc_hk_tm' : ['Time', 'Peregrine_Lock','Sensor_Temp_A','Sensor_Temp_B','Sensor_Temp_Duty_Cycle','Laser_Temp_A','Laser_Temp_B','Laser_Temp_Duty_Cycle','Laser_Current','Laser_Current_Zero_Cross','Microwave_Ref','Microwave_Ref_Zero_Cross','Zeeman_Freq_Zero_Cross','PCB_Temp_A','PCB_Temp_B','Laser_Diode_Voltage','Diode_Optical_Power','P2V4','P3V3','P8V','N8V','P12V','PCB_Temp_C','PCB_Temp_D','PCB_Temp_E','ICU_Checksum','ICU_Command_Count','ICU_Sync_Count']
-					   ,'pcu_data'  : ['Time','I_FIB','I_FOB','I_FSC','I_P3V3','I_FIBH','I_FOBH','I_FSCH','I_P1V8','Temp','V_P2V4','V_P3V3','V_P12V','V_P8V','V_N8V','V_P5V','V_N5V']}
 		super(packet_reciever, self).__init__()
 		self.port = serial_port 
 		self.logdir = logdir 		
@@ -72,190 +59,45 @@ class packet_reciever(Thread):
 		
 	def update_global_time(self): 
 		return datetime.datetime.now()
-	def update_files(self, t_str): 
-		self.files = {'fib_sci_tm': 'data/FIB_SCI_TM_' + t_str + '.csv', 'fob_sci_tm' : 'data/FOB_SCI_TM_' + t_str + '.csv', 'fsc_sci_tm' : 'data/FSC_SCI_TM_' + t_str + '.csv'
-					,'fib_hk_tm': 'data/FIB_HK_TM_' + t_str + '.csv', 'fob_hk_tm' : 'data/FOB_HK_TM_' + t_str + '.csv', 'fsc_hk_tm' : 'data/FSC_HK_TM_' + t_str + '.csv', 'pcu_data' : 'data/PCU' + t_str + '.csv'}
+	def update_files(self): 
+		self.files = {'fee_sci_tm': 'data/fee_science.log', 'house_keeping' : 'data/hk.log'}
 
 	def run(self): 
-		self.current_time = self.update_global_time()
-		self.update_files(self.current_time.strftime("%Y%m%d_%H%M%S"))
+		self.update_files()
 		s = fee_science(self.serial) 
 		h = hk_data(self.serial)
 		values = [self.hk_values, self.sci_values]
-		for key in self.files:
-			with open (self.files[key], 'a') as infile:  
-				infile.write("{}\n".format(','.join(self.labels[key])))
 		while self.start_running:
 			while self.serial.inWaiting(): 
 				decision_hk_sci = bytes(self.serial.read(size = 1))
-				counter = int.from_bytes(self.serial.read(size = 4), byteorder = 'big')
+				counter = bytes(self.serial.read(size = 4))
 				if decision_hk_sci[0] == 0: 
-					h.update(self.current_time, counter, values[0])
-				if decision_hk_sci[0] == 1: 
-					s.update(self.current_time, counter, values[1])
-				if(decision_hk_sci[0] == 1 or decision_hk_sci[0] == 0):
-					for key,f in values[decision_hk_sci[0]].items(): 
+					values[0]['house_keeping'] = decision_hk_sci 
+					h.update(values[0])
+				if decision_hk_sci[0] == 1:
+					values[1]['fee_sci_tm'] = decision_hk_sci
+					s.update(values[1])
+				for key,f in values[decision_hk_sci[0]].items(): 
 						with open(self.files[key], 'a') as infile: 
 							if f != '': 
-								infile.write("{}\n".format( f[:-1]))
+								infile.write("{}\n".format(binascii.hexlify(counter +  f[:-1])))
 
 
 class fee_science():
 		def __init__(self, port): 
 			self.port = port
-		def update(self, current_time, counter, values):
-			fee_packet = self.port.read(size = 3)
-			values["fib_sci_tm"] = ''
-			values["fob_sci_tm"] = '' 
-			values["fsc_sci_tm"] = ''
-			count_val = counter
-			for i in range(0, fee_packet[0]): 
-				f = fib_sci(self.port.read(size = 10))
-				time = (current_time + datetime.timedelta(seconds = count_val/128)).strftime("%Y-%m-%dT%H:%M:%S.%f")
-				values["fib_sci_tm"] += "{},".format(time) + f.update() + '\n'
-				count_val += 1
+		def update(self, values):
+			n_fee = self.port.read(size = 3)
+			values['fee_sci_tm'] += n_fee
+			for i in range(0, n_fee[0]): 
+				values['fee_sci_tm'] += self.port.read(size = 10) 
 			
-			count_val = counter	
-			for i in range(0, fee_packet[1]): 
-				f = fob_sci(self.port.read(size = 10))
-				time = (current_time + datetime.timedelta(seconds = count_val/128)).strftime("%Y-%m-%dT%H:%M:%S.%f")
-				values["fob_sci_tm"] += "{},".format(time) + f.update() + '\n' 
-				count_val += 1 
+			for i in range(0, n_fee[1]): 
+				values['fee_sci_tm'] += self.port.read(size = 10) 
 			
-			count_val = counter 
-			for i in range(0, fee_packet[2]):
-				f = fsc_sci(self.port.read(size = 11))
-				time = (current_time + datetime.timedelta(seconds = count_val/128)).strftime("%Y-%m-%dT%H:%M:%S.%f")
-				values["fsc_sci_tm"] += "{},".format(time) + f.update() + '\n'
-				count_val += 1
-
-def get_coord(data, byteorder): 
-	return "{},".format(int.from_bytes(data, byteorder, signed = True))
-
-def get_status(data): 
-	return "{},".format(data)
-					
-class fib_sci():  
-	def __init__(self, data): 
-		self.data = data
-	def update(self): 
-		data_stream =  get_coord(self.data[0:3], byteorder = 'big')  # x
-		data_stream += get_coord(self.data[3:6], byteorder = 'big')  # y
-		data_stream += get_coord(self.data[6:9], byteorder = 'big')  # z
-		data_stream += get_status(self.data[9]) # status
-		return data_stream
-	
-class fob_sci(): 
-	def __init__(self, data): 
-		self.data = data 
-	def update(self): 
-		data_stream =  get_coord(self.data[0:3], byteorder = 'little')
-		data_stream += get_coord(self.data[3:6], byteorder = 'little')
-		data_stream += get_coord(self.data[6:9], byteorder = 'little')
-		return data_stream 
-
-def get_sensor_laser(data): 
-	return "{},".format(data) 
-
-def get_laser_micro(data): 
-	return "{},".format(data) 
-
-def get_zeeman(data): 
-	return "{},".format(data)
-
-def get_sci_data_id(data): 
-	return "{},".format(data) 
-
-def get_sci_data(data):
-	return "{},".format(int.from_bytes(data,byteorder = 'big', signed = False))
-
-def get_time_stamp(data):
-	return "{}".format(int.from_bytes(data, byteorder = 'big', signed = False)) 
-class fsc_sci():
-	def __init__(self, data): 
-		self.data = data 
-	def update(self):
-		data_stream  = get_sensor_laser(self.data[0]) 
-		data_stream += get_laser_micro(self.data[1]) 
-		data_stream += get_zeeman(self.data[2]) 
-		data_stream += get_sci_data_id(self.data[3]) 
-		data_stream += get_sci_data(self.data[4:8]) 
-		data_stream += get_time_stamp(self.data[8:11])
-		return data_stream
-
-class pcu_data():
-	def __init__(self, data): 
-		self.data = data 
-	def update(self):
-		data_stream = ''
-		for i in filter(lambda w : w % 2 == 0, range(0, 32)): 
-			data_stream += "{},".format(int.from_bytes(self.data[i:i+2], byteorder = 'big'))
-		return data_stream 
-
-class fib_hk_data():		
-	def __init__(self, data): 
-		self.data = data
-	def update(self):
-		data_stream = '' 
-		for i in range(0, 3):
-			data_stream += "{},".format(int(self.data[i]))
-		for i in filter(lambda w : w % 2 == 0, range(1, 32)):
-			data_stream += ("{},".format(int.from_bytes(self.data[(i + 2): (i + 4)], byteorder = 'big')))
-		for i in range(33, len(self.data)): 
-			data_stream += "{},".format(int(self.data[i]))
-		return data_stream 
-
-class fob_hk_data():
-	def __init__(self, data): 
-		self.data = data
-	def update(self): 
-		data_stream = '' 
-		for i in range(0, len(self.data)): 
-			data_stream += ("{},".format(int.from_bytes(self.data[(i): (i + 2)], byteorder = 'big')))
-		return data_stream 
-			
-class fsc_hk_data():
-	def __init__(self, data): 
-		self.data = data 
-	def update(self): 
-		#extracting bytes 0 
-		data_stream = "{},".format(int(self.data[0]))
-			
-		#extracting bytes 1 - 2 and 3 - 4
-		for i in filter(lambda w : w % 2 == 0, range(1, 5)):
-			data_stream += "{},".format(int.from_bytes(self.data[i: (i + 2)], byteorder = 'big'))
-			
-		#extracting bytes 5-7
-		data_stream += "{},".format(int.from_bytes(self.data[5 : 7], byteorder = 'big'))
-			
-			#extracting bytes 7 - 8 and 9 - 10
-		for i in filter(lambda w : w % 2 == 0, range(7, 11)): 
-			data_stream += "{},".format(int.from_bytes(self.data[ i : (i + 2)], byteorder = 'big'))
-			
-		data_stream += "{},".format(int.from_bytes(self.data[11: 13], byteorder = 'big'))
-			
-			#extract bytes 13 - 15 and 16 - 18
-		for i in filter(lambda w : w % 3 == 1, range(13, 19)):
-			data_stream += "{},".format(int.from_bytes(self.data[i: (i + 3)], byteorder = 'big'))
-			
-		for i in filter(lambda w : w % 2 == 0, range(19, 23)):
-			data_stream += "{},".format(int.from_bytes(self.data[i:(i + 2)], byteorder = 'big'))
-		
-		data_stream += "{},".format(int.from_bytes(self.data[23:26], byteorder = 'big'))
-			
-		for i in filter(lambda w : w % 2 == 0, range(26, 30)):
-			data_stream += "{},".format(int.from_bytes(self.data[i : (i + 2)], byteorder = 'big'))
-				
-		data_stream += "{},".format(int.from_bytes(self.data[30 : 32], byteorder = 'big'))
-		data_stream  += "{},".format(int.from_bytes(self.data[32: 34], byteorder = 'big'))
-			
-		for i in filter(lambda w : w % 2 == 0, range(34, 50)):
-			data_stream += "{},".format(int.from_bytes(self.data[i : (i + 2)], byteorder = 'big'))
-					
-		for i in range(50, 53): 
-			data_stream  += 'a'
-		return data_stream
-		
+			for i in range(0, n_fee[2]):
+				values['fee_sci_tm'] += self.port.read(size = 11)
+											
 if __name__ == '__main__':
 		logging.basicConfig(filename='debugger.log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 		logger = logging.getLogger()
@@ -315,5 +157,3 @@ if __name__ == '__main__':
 			print(command)	
 		pkt_reciever.join()
 		print("program end")
-		
-		
