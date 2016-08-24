4e72384698d15c42cec5a4de1ec342cb67b508f3
aa18514 <aa18514@ic.ac.uk>
2016-08-24 14:48:24 +0100

    Update communication.ino

--- arduino/icu-simulator/communication.ino
+++ arduino/icu-simulator/communication.ino
@@ -1,4 +1,4 @@
- #include "pc_data_dump.h"
+#include "pc_data_dump.h"
   #define CMD_PACKET_SIZE 6 
    uint8_t default_fee_cmd[CMD_PACKET_SIZE] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x01}; 
    uint8_t fee_cmd[3][CMD_PACKET_SIZE]; 
@@ -9,20 +9,20 @@
     if(index == 0){
      pc_packet_arr[5] = BUFFER_SIZE;
       for(int l = 0; l < 10; l++, size_of_pc_packet++){
-        pc_packet_arr[size_of_pc_packet] = fib_pack[buffer_index].science_data[l];
+        pc_packet_arr[size_of_pc_packet] = fib_pack.science_data[l];
       }
     }
-     else if(index == 1){
+    if(index == 1){
      pc_packet_arr[6] = BUFFER_SIZE; 
       for(int l = 0; l < 10; l++, size_of_pc_packet++){
-        pc_packet_arr[size_of_pc_packet] = fob_pack[buffer_index].science_data[l];
+        pc_packet_arr[size_of_pc_packet] = fob_pack.science_data[l];
       }
      }
 
-     else if(index == 2){
+    if(index == 2){
      pc_packet_arr[7] = BUFFER_SIZE; 
-      for(int l = 0; l < 10; l++, size_of_pc_packet++){
-        pc_packet_arr[size_of_pc_packet] = fsc_pack[buffer_index].science_data[l];
+      for(int l = 0; l < 11; l++, size_of_pc_packet++){
+        pc_packet_arr[size_of_pc_packet] = fsc_pack.science_data[l];
       }
   }
   
@@ -30,7 +30,6 @@
  
  
   void process_packet(uint8_t index){                                                     //on every sync signal check to see if there is some processing to do and send the UART packet to the rest of the interfaces respectively. 
-      check_checksum(index); 
       if(response_packet_counter[index] > 0){
         create_pc_packet(index);  
         response_packet_counter[index] = 0; 
@@ -47,9 +46,10 @@
     else if(command[1] == 1){
       fee_cmd[index][0] |= 0x05;
     }
-    for(int i = 0; i < 5; i++){
-     fee_cmd[index][i] = command[i + 1];
-    }
+    fee_cmd[index][1] = command[2];
+    fee_cmd[index][2] = command[3];
+    fee_cmd[index][3] = command[4]; 
+    fee_cmd[index][4] = command[5];
     fee_cmd[index][5] = 0; /*here we will build our checksum */
     for(int i = 0; i < 5; i++){
       fee_cmd[index][5] ^= fee_cmd[index][i];
@@ -70,19 +70,19 @@
   void check_port(HardwareSerial* port, int index){
     if(port->available() > 0){
       if(index == 2){
-          fsc_pack[buffer_index].arr[response_packet_counter[2]] = port->read();
-          checksum[index] ^= fsc_pack[buffer_index].arr[response_packet_counter[2]]; 
+          fsc_pack.arr[response_packet_counter[2]] = port->read();
+          checksum[index] ^= fsc_pack.arr[response_packet_counter[2]]; 
           response_packet_counter[2]++;  
       }
       
       if(index == 1){
-          fob_pack[buffer_index].arr[response_packet_counter[1]] = port->read(); 
-          checksum[1] ^= fob_pack[buffer_index].arr[response_packet_counter[1]]; 
+          fob_pack.arr[response_packet_counter[1]] = port->read(); 
+          checksum[1] ^= fob_pack.arr[response_packet_counter[1]]; 
           response_packet_counter[1]++;
       }
       if(index == 0){
-          fib_pack[buffer_index].arr[response_packet_counter[0]] = port->read(); 
-          checksum[0] ^= fib_pack[buffer_index].arr[response_packet_counter[0]]; 
+          fib_pack.arr[response_packet_counter[0]] = port->read(); 
+          checksum[0] ^= fib_pack.arr[response_packet_counter[0]]; 
           response_packet_counter[0]++;
       }
     }
